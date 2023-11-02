2581b42e55f77b2b89cf9bf88701e144aada0ba8
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-02 18:21:34 -0400

    refactor event model

--- main_app/models.py
+++ main_app/models.py
@@ -2,11 +2,34 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+CATEGORIES = (
+  ('AR', 'Art'),
+  ('CH', 'Charity'),
+  ('CM', 'Comedy Show'),
+  ('CN', 'Conference'),
+  ('CO', 'Concert'),
+  ('CS', 'Class / Seminar'),
+  ('EX', 'Exercise / Fitness'),
+  ('FA', 'Fashion'),
+  ('FE', 'Festival'),
+  ('FI', 'Film'),
+  ('FO', 'Food'),
+  ('GA', 'Gala'),
+  ('MU', 'Music'),
+  ('NE', 'Networking'),
+  ('OT', 'Other'),
+  ('PA', 'Party'),
+  ('SP', 'Sports'),
+  ('TH', 'Theater'),
+  ('WE', 'Wedding'),
+  ('WR', 'Work Outing'),
+)
+
 class Event(models.Model):
   name = models.CharField(max_length=150)
   date = models.CharField(max_length=50)
   time = models.CharField(max_length=50)
-  category = models.CharField(max_length=50)
+  category = models.CharField(max_length=2, choices=CATEGORIES, default=[0][0])
   venue = models.CharField(max_length=150)
   address = models.TextField(max_length=250)
   cost = models.CharField(max_length=50)
@@ -14,8 +37,8 @@ class Event(models.Model):
   description = models.TextField(max_length=250)
 
   def __str__(self):
-      return self.name
+    return f"{self.get_category_display()} on {self.name}"
   
   def get_absolute_url(self):
-      return reverse("event-detail", kwargs={"event_id": self.id})
+    return reverse("event-detail", kwargs={"event_id": self.id})
   
