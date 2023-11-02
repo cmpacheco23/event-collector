4fb2c390a8381ba502d333d23c7cb7f3b82bf0ca
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-02 17:15:42 -0400

    refactor url in nav bar and create url path for events

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='home')
+  path('about/', views.about, name='about'),
+  path('events/', views.event_index, name='event-index')
 ]
