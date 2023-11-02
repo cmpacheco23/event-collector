6352a3ab724cc51757e772c3b997e5f72cf8303c
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-02 17:42:31 -0400

    refactor index template and create detail url

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('events/', views.event_index, name='event-index')
+  path('events/', views.event_index, name='event-index'),
+  path('events/<int:event_id>/', views.cat_detail, name='event-detail')
 ]
