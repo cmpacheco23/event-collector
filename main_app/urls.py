7e64507cacdc3dade650fbb8131b5c9d93975909
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-02 17:51:29 -0400

    create url for adding new event

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('events/', views.event_index, name='event-index'),
-  path('events/<int:event_id>/', views.event_detail, name='event-detail')
+  path('events/<int:event_id>/', views.event_detail, name='event-detail'),
+  path('events/create/', views.EventCreate.as_view(), name='event-create'),
 ]
