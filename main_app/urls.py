1faf135fa2e7c0ab91efd8bdde112ed41d38937c
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-02 17:58:11 -0400

    write urls for update and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('events/', views.event_index, name='event-index'),
   path('events/<int:event_id>/', views.event_detail, name='event-detail'),
   path('events/create/', views.EventCreate.as_view(), name='event-create'),
+  path('events/<int:pk>/update', views.EventUpdate.as_view(), name='event-update'),
+  path('events/<int:pk>/delete', views.EventDelete.as_view(), name='event-delete'),
 ]
