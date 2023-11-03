1bcb27e61297ccb45f43d348338fd088908aa3dd
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-02 20:13:58 -0400

    fix update form issues

--- main_app/views.py
+++ main_app/views.py
@@ -36,21 +36,20 @@ class EventCreate(CreateView):
     form.fields['time'].widget = forms.TimeInput(attrs={'type': 'time'})
     form.fields['cost'].widget = CurrencyInput(attrs={'placeholder': '$'})
     form.fields['address'].widget = AddressInput(attrs={"placeholder": '123 Main St, City, State, ZIP, Country'})
-
     return form
   
 class EventUpdate(UpdateView):
   model = Event
-  fields = ['name', 'date', 'time', 'category', 'venue', 'address', 'cost', 'celebration', 'description'] 
+  fields = '__all__'
 
   def get_form(self, form_class=None):
     form = super().get_form(form_class)
     form.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
     form.fields['time'].widget = forms.TimeInput(attrs={'type': 'time'})
     form.fields['cost'].widget = CurrencyInput(attrs={'placeholder': '$'})
-    form.fields['address'].widget = AddressInput(attrs={"placeholder": '123 Main St, City, State, ZIP,'})
-
-
+    form.fields['address'].widget = AddressInput(attrs={"placeholder": '123 Main St, City, State, ZIP, Country'})
+    return form
+  
 class EventDelete(DeleteView):
   model = Event
   success_url = '/events/'
