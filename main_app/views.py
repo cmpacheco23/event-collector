9fe8893531b006065c779e18284c0942614c2aad
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-11-02 20:48:45 -0400

    write url and view signup function

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django import forms
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Event
 # Create your views here.
 
@@ -59,3 +60,17 @@ class EventUpdate(UpdateView):
 class EventDelete(DeleteView):
   model = Event
   success_url = '/events/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('event-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+      form = UserCreationForm()
+      context = {'form': form, 'error_message': error_message}
+      return render(request, 'signup.html', context)
